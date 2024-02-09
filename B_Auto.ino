859b3d76b574a720184a322ad325f9f2722b29e3
rollocraft <Rollocraft117@gmail.com>
2024-02-09 12:56:48 +0100

    Version 1.0

--- B_Auto.ino
+++ B_Auto.ino
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-// Variablen
 int triggerVorne = 8;
 int echoVorne = 9;
 
@@ -20,35 +19,61 @@ int motorIN2 = A1;
 int motorIN3 = A2;
 int motorIN4 = A3;
 
-int LDR = A5;
-
-int LedWeiss = A4;
-int LedGelb = 13;
-int LedRot = 12;
-
-int piezo = 11;
-
-int entfernungLinksOld;
-int entfernungLinks;
-int entfernungHinten;
-int entfernungRechts;
-int entfernungVorne;
-
 int returnwert;
 
-int run;
+int run = 0;
 
 int maxEntfernung = 200;
 
 int Zeit;
 
-int turn = 620;
-int turn_dif = 545;
+void TurnRight(int time) {
+  digitalWrite(motorIN1, HIGH);
+  digitalWrite(motorIN2, LOW);
+  digitalWrite(motorIN3, LOW);
+  digitalWrite(motorIN4, HIGH);
+  analogWrite(motorENA, 100);
+  analogWrite(motorENB, 100);
+  delay(time);
+}
 
-// setup
+void TurnLeft(int time) {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, HIGH);
+  digitalWrite(motorIN3, HIGH);
+  digitalWrite(motorIN4, LOW);
+  analogWrite(motorENA, 100);
+  analogWrite(motorENB, 100);
+  delay(time);
+}
 
-void setup()
-{
+void Forward() {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, HIGH);
+  digitalWrite(motorIN3, LOW);
+  digitalWrite(motorIN4, HIGH);
+  analogWrite(motorENA, 200);
+  analogWrite(motorENB, 200);
+}
+
+void Backward() {
+  digitalWrite(motorIN1, HIGH);
+  digitalWrite(motorIN2, LOW);
+  digitalWrite(motorIN3, HIGH);
+  digitalWrite(motorIN4, LOW);
+  analogWrite(motorENA, 200);
+  analogWrite(motorENB, 200);
+}
+
+void Stop() {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, LOW);
+  digitalWrite(motorIN3, LOW);
+  digitalWrite(motorIN4, LOW);
+  analogWrite(motorENA, 0);
+  analogWrite(motorENB, 0);
+}
+void setup() {
   pinMode(motorENA, OUTPUT);
   pinMode(motorENB, OUTPUT);
   pinMode(motorIN1, OUTPUT);
@@ -66,149 +91,34 @@ void setup()
   pinMode(echoRechts, INPUT);
   pinMode(echoHinten, INPUT);
 
-  pinMode(LDR, INPUT);
-
-  pinMode(piezo, OUTPUT);
-
-  pinMode(LedWeiss, OUTPUT);
-  pinMode(LedGelb, OUTPUT);
-  pinMode(LedRot, OUTPUT);
-
   Serial.begin(9600);
 }
 
-void forward()
-{
-  digitalWrite(motorIN1, LOW);
-  digitalWrite(motorIN2, HIGH);
-  digitalWrite(motorIN3, LOW);
-  digitalWrite(motorIN4, HIGH);
-  analogWrite(motorENA, 200);
-  analogWrite(motorENB, 200);
-}
-
-void backward()
-{
-  digitalWrite(motorIN1, HIGH);
-  digitalWrite(motorIN2, LOW);
-  digitalWrite(motorIN3, HIGH);
-  digitalWrite(motorIN4, LOW);
-  analogWrite(motorENA, 200);
-  analogWrite(motorENB, 200);
-}
-
-void turnRight(int time)
-{
-  digitalWrite(motorIN1, HIGH);
-  digitalWrite(motorIN2, LOW);
-  digitalWrite(motorIN3, LOW);
-  digitalWrite(motorIN4, HIGH);
-  analogWrite(motorENA, 100);
-  analogWrite(motorENB, 100);
-  delay(12);
-}
-
-void turnLeft(int time)
-{
-  digitalWrite(motorIN1, LOW);
-  digitalWrite(motorIN2, HIGH);
-  digitalWrite(motorIN3, HIGH);
-  digitalWrite(motorIN4, LOW);
-  analogWrite(motorENA, 100);
-  analogWrite(motorENB, 100);
-  delay(12);
-}
-
-void stop()
-{
-  digitalWrite(motorIN1, LOW);
-  digitalWrite(motorIN2, LOW);
-  digitalWrite(motorIN3, LOW);
-  digitalWrite(motorIN4, LOW);
-  analogWrite(motorENA, 0);
-  analogWrite(motorENB, 0);
-}
-
-void LDRM()
-{
-  int LDR_Wert = analogRead(LDR);
-
-  if (!LDR_Wert > 400)
-  {
-    digitalWrite(LedWeiss, HIGH);
-  }
-  else
-  {
-    digitalWrite(LedWeiss, LOW);
-  }
-}
-
-void loop()
-{
-  LDRM();
-
+void loop() {
   int entfernungVorne = messeEntfernung(triggerVorne, echoVorne);
-  int entfernungLinks = messeEntfernung(triggerLinks, echoLinks);
 
-  if (entfernungVorne > 20)
-  { //Falls der Roboter nicht gerade fährt
-    if (entfernungLinks != entfernungLinksOld)
-    { // Gleicht es aus
-      if (entfernungLinks > entfernungLinksOld)
-      {
-        Serial.println("Moving Left");
-        turnLeft(turn);
-        Serial.println("done");
-      }
-      else
-      {
-        Serial.println("Moving Right");
-        turnRight(turn);
-        Serial.println("done");
-      }
-      //Updaten der neuen entfernung Links
-      entfernungLinksOld = entfernungLinks;
-    }
-    forward();
-  }
-  else if (entfernungRechts > 30 || entfernungLinks > 30)
-  {
-    // Messungen von Rechts und Hitnen erst jetzt um Resourcen zu sparen
-    int entfernungRechts = messeEntfernung(triggerRechts, echoRechts);
-    int entfernungHinten = messeEntfernung(triggerHinten, echoHinten);
-
-    if (entfernungRechts > entfernungLinks)
-    {   
-        Serial.println("Moving Right");
-        turnRight(turn);
-        Serial.println("done");
-    }
-    else
-    {
-        Serial.println("Moving Left");
-        turnLeft(turn);
-        Serial.println("done");
-        run = 0;    
-    }
-
-    // Nach der Drehung die Entfernung Links auf die neue Updaten 
+  if (entfernungVorne > 20) {
+    Forward();
+  } else {
+    Stop();
+    
+    //Messe Entfernung Links und Rechts und fahre in die Richtung mit mehr Platz
     int entfernungLinks = messeEntfernung(triggerLinks, echoLinks);
-    entfernungLinksOld = entfernungLinks;
+    int entfernungRechts = messeEntfernung(triggerRechts, echoRechts);
 
-  }
-  else if (entfernungHinten > 20)
-  {
-    backward();
-  }
-  else
-  {
-    stop();
-    tone(piezo, 1000);
+    if (entfernungRechts > entfernungLinks) {
+      Serial.println("Moving Right");
+      TurnRight(480);
+      Serial.println("done");
+    } else {
+      Serial.println("Moving Left");
+      TurnLeft(480);
+      Serial.println("done");
+    }
   }
 }
 
-int messeEntfernung(int triggerPin, int echoPin)
-{
+int messeEntfernung(int triggerPin, int echoPin) {
   digitalWrite(triggerPin, LOW);
   delay(5);
   digitalWrite(triggerPin, HIGH);
@@ -216,5 +126,6 @@ int messeEntfernung(int triggerPin, int echoPin)
   digitalWrite(triggerPin, LOW);
   long Zeit = pulseIn(echoPin, HIGH);
   returnwert = (Zeit / 2) * 0.03432;
-  return returnwert; //gibt die Entfernung in cm zurück
+
+  return returnwert; //Returnt die Entfernung in cm
 }
